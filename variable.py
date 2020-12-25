e1d3389a35779033e3f6edd5aab6b01fcb0368ac
Farjana Alam Pushpo <70435827+Farjana-Alam-Pushpo@users.noreply.github.com>
2020-12-25 20:00:58 +0600

    Add files via upload
    py

---
+++ variable.py
@@ -0,0 +1,23 @@
+a = "Pushpo"
+# a = 'Pushpo'
+# a = '''Pushpo'''
+b = 708
+c = 45.91
+d = True
+e = None 
+# printing the variable
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# printing the type of variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+
