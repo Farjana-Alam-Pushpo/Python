94e4043bc1980e57fe4eb28b7badd1af92097391
Farjana Alam Pushpo <70435827+Farjana-Alam-Pushpo@users.noreply.github.com>
2020-12-25 22:13:24 +0600

    Add files via upload
    py

---
+++ operators.py
@@ -0,0 +1,44 @@
+a = 13
+b = 20
+
+# Arithmatic operator 
+print("sum",3+4)
+print("sum4",3-4)
+print("sum1",3*4)
+print("sum2",3/4)
+print("sum3",3%4)
+
+# Assignment operators
+a = 20
+b = 10
+a+=12
+b-=2
+b+=2
+b*=2
+b/=2
+print(a)
+print(b)
+
+# compasion operators
+#b = (14<=7)
+#b = (14>=7)
+#b = (14<=7)
+#b = (14<7)
+#b = (14>7)
+#b = (1==7)
+b = (14!=7)
+print(b)
+
+# Logical operators
+bool1= True
+bool2= False
+#print( bool1 and bool2 )
+#print( bool1 or bool2 )
+print(  not bool2 )
+
+
+
+
+
+
+
